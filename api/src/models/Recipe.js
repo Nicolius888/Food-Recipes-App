7def6f2d81ea6fe8ecdb90e7a5f4e5d60771b6d8
nicolius888 <nicola.edgardo@gmail.com>
2022-06-09 18:19:34 -0300

    update model recipe, score no longer available

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -18,10 +18,7 @@ module.exports = (sequelize) => {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    score: {
-      type: DataTypes.INTEGER,
-    },
-    healtScore: {
+    healthScore: {
       type: DataTypes.INTEGER,
     },
     steps: {
