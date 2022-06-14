97f0d238891188b825ad4b0464440fee880ddab8
nicolius888 <nicola.edgardo@gmail.com>
2022-06-13 23:38:30 -0300

    configure store updated!

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,16 @@
-import { configureStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
+import {configureStore} from '@reduxjs/toolkit';
+// import { applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import thunk from "redux-thunk";
 import rootReducer from "../reducer";
 
-export const store = configureStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+
+export const store = configureStore({
+  reducer: rootReducer,
+})
+
+// export const store = configureStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
 //check how works configure store new method
