565f8e255481f2cdadede0465126a425f946bd2e
nicolius888 <nicola.edgardo@gmail.com>
2022-06-13 22:51:27 -0300

    checkout new feat configureStore

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
+import { configureStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer";
 
-export const store = createStore(
+export const store = configureStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+//check how works configure store new method
